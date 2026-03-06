139d1017a88220bc1a55cc928742525efe757c12
najwa13 <najwafathi05@hotmail.com>
2026-03-06 14:44:57 +0000

    Modif Challenge #3: The Debt TrackerBrief

--- Arrays-Strings/challenge3.php
+++ Arrays-Strings/challenge3.php
@@ -1,15 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html >
 <body>
     <?php 
     $debts = ["Amine"=>"150","Ali"=>"100","Sara"=>"200","ahmed"=>"60","nora"=>"90"];
     $total=0;
-    $highdebts=[];
+    
     foreach ($debts as $friend => $amount){
 
         $total+=$amount;
